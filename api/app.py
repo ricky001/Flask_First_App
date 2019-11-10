f4b61e1f538549fe8564e01356bbaec7de77cf74
somya.chawla <somyachawlacs2008@gmail.com>
2019-11-10 17:36:51 +0530

    Modified the app.py file and added the login.html in templated folder

--- api/app.py
+++ api/app.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
 
-@app.route("/<string:name>")
-def home(name):
-    return f"Welcome {name}"
+@app.route("/login",methods=["POST","GET"])
+def home():
+    if request.method == "POST":
+        print((request.form['nm']))
+        return redirect(url_for('about',name = request.form['nm']))
+    elif request.method == "GET":
+        return render_template("login.html")
 
-app.run(debug=True)
+    #render_template("login.html")
+def about(name):
+    return f"<h1>'Welcome to About Page' {name} <h1>"
+app.add_url_rule('/about/<name>','about',about)
+
+if __name__=="__main__":
+    app.run(debug=True)
